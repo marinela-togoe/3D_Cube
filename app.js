d41098a23409ae62b6317fcee312d7f1a176b995
Marinela <marinelatogoe@gmail.com>
2022-05-12 23:20:59 +0200

    Added JavaScript functionality for the play button using setInterval() and clearInterval() in order to rotate the cube and stop it on click

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-let x = 0;
+let x = 0, bool = false, interval;
+
 
 const rotate = () => {
   const cubes = document.querySelectorAll(".cube");
   Array.from(cubes).forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
+};
+
+const changePlayPause = () => {
+  const i = document.querySelector(".play-pause i");
+  const cls = i.classList;
+  console.log(cls);
+}
+
+const playPause = () => {
+  if(!bool) {
+    interval = setInterval(() => {
+      x -= 90;
+      rotate();
+    }, 2000);
+    bool = true;
+  } else {
+    clearInterval(interval);
+    bool = false;
+  };
+
 };
 
 document.querySelector(".left-arrow").addEventListener("click", () => {
@@ -33,4 +54,9 @@ document.querySelector(".right-arrow").addEventListener("mouseover", () => {
 document.querySelector(".right-arrow").addEventListener("mouseout", () => {
   x += 25;
   rotate();
+});
+
+document.querySelector(".play-pause").addEventListener("click", () => {
+    playPause();
+    changePlayPause();
 });
