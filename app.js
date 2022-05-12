039443efff4f55f0fdab516b579348ef5997b972
Marinela <marinelatogoe@gmail.com>
2022-05-12 22:46:02 +0200

    Added JavaScript functionality for the right arrow

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 let x = 0;
 
+const rotate = () => {
+  const cubes = document.querySelectorAll(".cube");
+  Array.from(cubes).forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
+};
 
 document.querySelector(".left-arrow").addEventListener("click", () => {
   x += 90;
-  const cubes = document.querySelectorAll(".cube");
-  Array.from(cubes).forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
-  console.log(x);
+  rotate();
+});
 
+document.querySelector(".left-arrow").addEventListener("mouseover", () => {
+  x += 25;
+  rotate();
+});
+
+document.querySelector(".left-arrow").addEventListener("mouseout", () => {
+  x -= 25;
+  rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("click", () => {
+  x -= 90;
+  rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("mouseover", () => {
+  x -= 25;
+  rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("mouseout", () => {
+  x += 25;
+  rotate();
 });
